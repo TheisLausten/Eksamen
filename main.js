2c87e07e83a1ca43939e90cb1c9b38369bed8e93
TheisLausten <theislausten02@gmail.com>
2025-12-18 11:01:09 +0100

    Java script for hero

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function myAlert() {
-    alert ("Det her virker");
+    const name = document.querySelector(".navn").value;
+    const tlf = document.querySelector(".tlfnr").value;
+    const email = document.querySelector(".email").value;
+    alert ("Det her dit information \n" + name + "\n" + tlf +"\n" + email ); 
 }
