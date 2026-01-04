2b4519611710fcb52e6e82b5d33a49b366dcac07
TheisLausten <theislausten02@gmail.com>
2026-01-04 14:10:10 +0100

    Interactive nav bar 1

--- main.js
+++ main.js
@@ -4,13 +4,3 @@ function myAlert() {
     const email = document.querySelector(".email").value;
     alert ("Det her dit information \n" + name + "\n" + tlf +"\n" + email ); 
 }
-
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-menu');
-let menuOpen = false;
-
-hamburger.addEventListener('click', mobileMenu)
-function mobileMenu() {
-    hamburger.classList.toggle('active')
-    navMenu.classList.toggle('active')
-}
