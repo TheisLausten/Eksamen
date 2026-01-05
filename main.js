ef822c85b3b542214e73390c0dbeeecd88b63aa0
TheisLausten <theislausten02@gmail.com>
2026-01-05 09:24:00 +0100

    Java script opdate

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ function myAlert() {
     const name = document.querySelector(".navn").value;
     const tlf = document.querySelector(".tlfnr").value;
     const email = document.querySelector(".email").value;
-    alert ("Det her din information \n" + name + "\n" + tlf +"\n" + email ); 
+    alert ("Det her er din information \n" + name + "\n" + tlf +"\n" + email ); 
 }
