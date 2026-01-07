bca34996251cff002e1208545566f7032cebbca3
TheisLausten <theislausten02@gmail.com>
2026-01-07 15:50:25 +0100

    Menu picture delete

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ function myAlert() {
     const email = document.querySelector(".email").value;
     alert ("Det her er din information \n" + name + "\n" + tlf +"\n" + email ); 
 }
+
+function myFunction() {
+  var x = document.getElementById("nav-menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
